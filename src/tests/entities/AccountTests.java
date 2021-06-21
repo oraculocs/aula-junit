70a88b1d1aa36aeda815022e3c3533fb7abe4151
Carlos Augusto <augustino2003@hotmail.com>
2021-06-21 16:50:34 -0300

    Teste saque full

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -33,4 +33,15 @@ public class AccountTests {
 		
 	}
 
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
 }
