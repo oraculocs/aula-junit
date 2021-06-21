eca27fd128be8d3f6e31342ebb310f93193b239c
Carlos Augusto <augustino2003@hotmail.com>
2021-06-21 17:01:58 -0300

    Testando os 2 cenários do método withdraw

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -44,4 +44,24 @@ public class AccountTests {
 		Assertions.assertTrue(expectedValue == acc.getBalance());
 		Assertions.assertTrue(result == initialBalance);
 	}
+	
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+		
+		Account acc = AccountFactory.createAccount(800.0);
+		
+		acc.withdraw(500);
+		
+		Assertions.assertEquals(300.0, acc.getBalance());
+	}
+	
+	@Test
+	public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account acc = AccountFactory.createAccount(800.0);
+			
+			acc.withdraw(801.0);
+		});
+	}
 }
